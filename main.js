82543ab8119122856da18bd640572f61a2b86280
nato <natomtiuli@protonmail.com>
2020-02-26 18:47:46 +0400

    4

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+
 function openForm() {
   document.getElementById("myForm").style.display = "block";
-  
+
 }
 
 function closeForm() {
@@ -10,9 +11,26 @@ function closeForm() {
 var list=document.getElementById('demo');
 
 function addTask(task) {
+  var ba=document.getElementById("num").innerHTML;
+  ba=+ba;
+  ++ba;
+  document.getElementById("num").innerHTML=ba;
   // document.getElementById('task') ver naxulobda. inputs id ar qonda minichebuli
   var t = document.getElementById('task').value; // aq shegidzlia pirdapir task.value - s mimarto
   var entry = document.createElement('li');
   entry.appendChild(document.createTextNode(t));
   list.appendChild(entry);
+  
+}
+
+function viewList(){
+  if(document.getElementById("view1").innerHTML=="View List"){
+    document.getElementById("myList").style.display="block";
+    document.getElementById("view1").innerHTML="Hide list";
+  }
+  else{
+    document.getElementById("myList").style.display="none";
+    document.getElementById("view1").innerHTML="View List";
+  }
+
 }
