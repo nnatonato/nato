10a80eed8f862bc2c1c54ac66a5534c008e0df4b
Nika chkhobadze <chkhobadze@nomachine.com>
2020-02-26 16:14:37 +0400

    fixed form issue

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }  
-  var list=document.getElementById('demo');
-  function addTask(task){
-    var t = document.getElementById('task').value;
-    var entry = document.createElement('li');
-    entry.appendChild(document.createTextNode(t));
-    list.appendChild(entry);
-  }
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+var list=document.getElementById('demo');
+
+function addTask(task) {
+  // document.getElementById('task') ver naxulobda. inputs id ar qonda minichebuli
+  var t = document.getElementById('task').value; // aq shegidzlia pirdapir task.value - s mimarto
+  var entry = document.createElement('li');
+  entry.appendChild(document.createTextNode(t));
+  list.appendChild(entry);
+}
